9ecd80111df155ae96b62b166abdea4ff0ad8315
Vitor Nere <vitor.nere@hotmail.com>
2021-09-18 12:21:45 -0300

    Basic app

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Hello(name) {
+    return "Hello, " + name;
+}
+exports.default = Hello;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function Hello(name: string) {
+    return "Hello, " + name;
+}
+
+export default Hello;
