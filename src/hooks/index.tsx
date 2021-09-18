21ca869ba8cc57099d155070727e74e0bf20c50e
Vitor Nere <vitor.nere@hotmail.com>
2021-09-18 11:46:54 -0300

    reset

--- src/hooks/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { PyodideProvider } from './pyodide';
-
-const AppProvider: React.FC = ({ children }) => (
-  <PyodideProvider>{children}</PyodideProvider>
-);
-
-export default AppProvider;
