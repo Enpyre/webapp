46e4b470b66e385a21c05148bc0269dae86e07a4
Lucas Amoêdo <lucas.advc@gmail.com>
2021-09-18 12:47:41 -0300

    Add basic draw functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,25 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Hello(name) {
-    return "Hello, " + name;
-}
-exports.default = Hello;
+var _window = window;
+var run = function () {
+    var textarea = document.getElementById("code");
+    var code = textarea === null || textarea === void 0 ? void 0 : textarea.value;
+    console.log(_window.pyodide.runPython(code));
+};
+// graphics.js?
+var app = undefined;
+var drawCanvas = function (x, y) {
+    app = new _window.PIXI.Application({ width: x, height: y });
+    var canvasDiv = document.getElementById("app");
+    canvasDiv.innerHTML = '';
+    canvasDiv.appendChild(app.view);
+};
+var drawCircle = function (x, y, r, color) {
+    var graphics = new _window.PIXI.Graphics();
+    graphics.lineStyle(0);
+    graphics.beginFill(color, 1);
+    graphics.drawCircle(x, y, r);
+    graphics.endFill();
+    app.stage.addChild(graphics);
+};
+_window.drawCanvas = drawCanvas;
+_window.drawCircle = drawCircle;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-function Hello(name: string) {
-    return "Hello, " + name;
+const _window: any = window;
+
+const run = () => {
+  const textarea: any = document.getElementById("code");
+  const code = textarea?.value;
+  console.log(_window.pyodide.runPython(code));
 }
 
-export default Hello;
+// graphics.js?
+
+let app: any = undefined;
+
+const drawCanvas = (x: number, y: number) => {
+    app = new _window.PIXI.Application({ width: x, height: y });
+    const canvasDiv: any = document.getElementById("app");
+    canvasDiv.innerHTML = '';
+    canvasDiv.appendChild(app.view);
+}
+
+const drawCircle = (x: number, y: number, r: number, color: number) => {
+    const graphics = new _window.PIXI.Graphics()
+
+    graphics.lineStyle(0);
+    graphics.beginFill(color, 1);
+    graphics.drawCircle(x, y, r);
+    graphics.endFill();
+
+    app.stage.addChild(graphics);
+}
+
+_window.drawCanvas = drawCanvas;
+_window.drawCircle = drawCircle;
